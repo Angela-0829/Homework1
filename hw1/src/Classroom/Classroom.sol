e3752ada30b56096ad29971c507de90b8b4d84ac
angela <c550006742@gmail.com>
2024-03-21 10:48:50 +0800

    finish Classroom

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,17 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    bool public isEnrolled;
 
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if (isEnrolled == false){
+            isEnrolled = true;
+            return 1000;
+        }
+        else{
+            return 123;
+        }
     }
 }
 
@@ -18,6 +26,12 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if (IClassroomV2(msg.sender).isEnrolled() == false){
+            return 1000;
+        }
+        else{
+            return 123;
+        }
     }
 }
 
@@ -25,5 +39,11 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if (gasleft() > 7000){
+            return 1000;
+        }
+        else{
+            return 123;
+        }
     }
 }
